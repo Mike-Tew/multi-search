2acd7ca258311ea576533c553e538fbe4ad50b84
Michael Tew <mike.tew@gmail.com>
2021-09-10 11:02:47 -0700

    Initial GUI.

---
+++ main.py
@@ -0,0 +1,45 @@
+from tkinter import Tk, LabelFrame, Entry, Button, Label, Checkbutton, END
+from time import sleep
+import webbrowser
+
+
+class Gui(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Quick Web Search")
+        self.geometry("+1000+300")
+
+        self.search_engines = {
+            "Google": "https://www.google.com/search?q=PARAM",
+            "GoodReads": "https://www.goodreads.com/search?utf8=%E2%9C%93&q=PARAM&search_type=books&search%5Bfield%5D=on",
+            "Audible": "https://www.audible.com/search?keywords=PARAM&ref=a_search_t1_header_search",
+            "Amazon": "https://www.amazon.com/s?k=PARAM&ref=nb_sb_noss",
+        }
+
+        self.search_frame = LabelFrame(self, text="Search")
+        self.search_frame.grid(row=0, column=0, padx=10, pady=10)
+        self.search_box = Entry(self.search_frame)
+        self.search_box.grid(row=0, column=0, padx=10, pady=10)
+        self.search_box.bind("<Return>", lambda x: self.perform_search())
+        self.search_box.focus_set()
+        self.search_button = Button(
+            self.search_frame, text="SEARCH", command=self.perform_search
+        )
+        self.search_button.grid(row=0, column=1, padx=[0, 10])
+
+    def perform_search(self):
+        search_params = self.search_box.get().strip()
+        self.search_box.delete(0, END)
+        if len(search_params) != 0:
+            format_param = "+".join(search_params.split())
+            for engine in self.search_engines.values():
+                search_string = engine.replace("PARAM", format_param)
+                webbrowser.open(search_string)
+                sleep(0.05)
+
+            self.destroy()
+
+
+if __name__ == "__main__":
+    gui = Gui()
+    gui.mainloop()
