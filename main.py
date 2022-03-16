5c8dcdffeb50b3bdb37a1cab7bacaff6e61dad23
Michael Tew <mike.tew@gmail.com>
2022-03-16 02:39:45 -0700

    WIP: Implementing SearchEngine dataclass.

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 import tkinter as tk
-from tkinter import ttk
-from time import sleep
 import webbrowser
+from dataclasses import dataclass
+from time import sleep
+from tkinter import ttk
+
+
+@dataclass
+class SearchEngine:
+    """Dataclass for storing search engine information."""
+
+    name: str
+    search_str: str
+    category: str
+
+
+search_engines = [
+    SearchEngine("Google", "https://www.google.com/search?q=PARAM", "search"),
+    SearchEngine(
+        "GoodReads",
+        "https://www.goodreads.com/search?utf8=%E2%9C%93&q=PARAM&search_type=books&search%5Bfield%5D=on",
+        "search",
+    ),
+    SearchEngine(
+        "Audible",
+        "https://www.audible.com/search?keywords=PARAM&ref=a_search_t1_header_search",
+        "search",
+    ),
+    SearchEngine("Amazon", "https://www.amazon.com/s?k=PARAM&ref=nb_sb_noss", "search"),
+]
 
 
 class Gui(tk.Tk):
@@ -22,10 +48,10 @@ class Gui(tk.Tk):
         self.search_frame.grid(row=0, column=0, padx=10, pady=10)
         self.search_box = tk.Entry(self.search_frame)
         self.search_box.grid(row=0, column=0, padx=10, pady=10)
-        self.search_box.bind("<Return>", lambda x: self.perform_search())
+        self.search_box.bind("<Return>", lambda x: self._on_search())
         self.search_box.focus_set()
         self.search_button = tk.Button(
-            self.search_frame, text="SEARCH", command=self.perform_search
+            self.search_frame, text="SEARCH", command=self._on_search
         )
         self.search_button.grid(row=0, column=1, padx=[0, 10])
 
@@ -41,23 +67,25 @@ class Gui(tk.Tk):
             self.engine_frame, text="Clear", command=self.show_values
         )
         self.clear_button.pack()
-        print(self.checkbox_dict)
+        # print(self.checkbox_dict)
 
     def show_values(self):
         print(self.checkbox_dict["Google"].get())
         print(self.checkbox_dict["Amazon"])
 
-    def perform_search(self):
+    def _on_search(self):
         search_params = self.search_box.get().strip()
-        self.search_box.delete(0, tk.END)
-        if len(search_params) != 0:
-            format_param = "+".join(search_params.split())
-            for engine in self.search_engines.values():
-                search_string = engine.replace("PARAM", format_param)
-                webbrowser.open(search_string)
-                sleep(0.05)
+        if len(search_params) == 0:
+            return
 
-            self.destroy()
+        self.search_box.delete(0, tk.END)
+        format_param = "+".join(search_params.split())
+        for engine in self.search_engines.values():
+            search_string = engine.replace("PARAM", format_param)
+            webbrowser.open(search_string)
+            sleep(0.05)
+
+        self.destroy()
 
 
 if __name__ == "__main__":
