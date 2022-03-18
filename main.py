3d7fdf2c297b064b34a70bcc13bd466475174e27
Michael Tew <mike.tew@gmail.com>
2022-03-17 22:17:12 -0700

    WIP: Adding categories.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 from time import sleep
 from tkinter import ttk
 
-from search_engines import search_engines
+from search_engines import categories, search_engines
 
 
 @dataclass
@@ -15,9 +15,12 @@ class SearchEngine:
     search_str: str
     category: str
 
+    def __post_init__(self):
+        self.variable = tk.IntVar()
+
 
 class Gui(tk.Tk):
-    def __init__(self, search_engines):
+    def __init__(self, categories, search_engines):
         super().__init__()
         self.title("Quick Web Search")
         self.geometry("+1000+300")
@@ -40,36 +43,41 @@ class Gui(tk.Tk):
             SearchEngine(engine["name"], engine["search str"], engine["category"])
             for engine in search_engines
         ]
+
         for engine in self.search_engines:
-            engine.variable = tk.IntVar()
             tk.Checkbutton(
                 self.engine_frame, text=engine.name, variable=engine.variable
             ).pack()
 
         self.clear_button = tk.Button(
-            self.engine_frame, text="Clear", command=self.show_values
+            self.engine_frame, text="Clear", command=self._on_clear
         )
         self.clear_button.pack()
+        for engine in self.search_engines:
+            print(engine)
 
-    def show_values(self):
-        print(self.checkbox_dict["Google"].get())
-        print(self.checkbox_dict["Amazon"])
+    def _on_clear(self):
+        print(self.search_engines[0].variable.get())
 
     def _on_search(self):
         search_params = self.search_box.get().strip()
+        self.search_box.delete(0, tk.END)
         if len(search_params) == 0:
             return
 
-        self.search_box.delete(0, tk.END)
-        format_param = "+".join(search_params.split())
-        for engine in self.search_engines.values():
-            search_string = engine.replace("PARAM", format_param)
-            webbrowser.open(search_string)
-            sleep(0.05)
+        for engine in self.search_engines:
+            print(engine.variable.get())
+            if engine.variable.get():
+                format_param = "+".join(search_params.split())
+                search_string = engine.search_str.replace("PARAM", format_param)
+                webbrowser.open(search_string)
+                sleep(0.05)
 
-        self.destroy()
+        # self.destroy()
+
+
 
 
 if __name__ == "__main__":
-    gui = Gui(search_engines)
+    gui = Gui(categories, search_engines)
     gui.mainloop()
--- search_engines.py
+++ search_engines.py
@@ -1,22 +1,24 @@
+categories = ["Search", "Shopping", "Books"]
+
 search_engines = [
     {
         "name": "Google",
         "search str": "https://www.google.com/search?q=PARAM",
-        "category": "search",
+        "category": "Search",
     },
     {
         "name": "GoodReads",
         "search str": "https://www.goodreads.com/search?utf8=%E2%9C%93&q=PARAM&search_type=books&search%5Bfield%5D=on",
-        "category": "search",
+        "category": "Search",
     },
     {
         "name": "Audible",
         "search str": "https://www.audible.com/search?keywords=PARAM&ref=a_search_t1_header_search",
-        "category": "search",
+        "category": "Search",
     },
     {
         "name": "Amazon",
         "search str": "https://www.amazon.com/s?k=PARAM&ref=nb_sb_noss",
-        "category": "search",
+        "category": "Search",
     },
 ]
