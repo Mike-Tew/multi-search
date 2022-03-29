74655b206b23f75aaea12665f8ec323efdec6608
Michael Tew <mike.tew@gmail.com>
2022-03-29 03:25:33 -0700

    Improved styling.

--- quick-web-search.py
+++ quick-web-search.py
@@ -9,6 +9,7 @@ from utils.category_frame import CategoryFrame
 
 from pprint import pprint
 
+
 class Gui(tk.Tk):
     def __init__(self, categories, search_engines):
         super().__init__()
@@ -20,12 +21,20 @@ class Gui(tk.Tk):
         self.search_frame = ttk.LabelFrame(self, text="Search")
         self.search_frame.grid(row=0, column=0, padx=10, pady=10)
         self.search_string = tk.StringVar()
-        self.search_box = ttk.Entry(self.search_frame, textvariable=self.search_string)
+        self.search_box = ttk.Entry(
+            self.search_frame,
+            textvariable=self.search_string,
+            width=25,
+            font=("Helvetica", 14),
+        )
         self.search_box.grid(row=0, column=0, padx=10, pady=10)
         self.search_box.focus_set()
 
         self.search_button = ttk.Button(
-            self.search_frame, text="SEARCH", command=self._on_search
+            self.search_frame,
+            text="SEARCH",
+            command=self._on_search,
+            style="Search.TButton",
         )
         self.search_button.grid(row=0, column=1, padx=[0, 10])
 
@@ -35,9 +44,9 @@ class Gui(tk.Tk):
         self.scale_bar.grid(row=0, column=0, padx=30, pady=5)
         self.scale_bar.set(0.5)
 
-        ttk.Button(self, text="Clear All", command=self._on_style).grid(
-            row=0, column=2, sticky="W"
-        )
+        ttk.Button(
+            self, text="Clear All", command=self._on_clear, style="Clear.TButton"
+        ).grid(row=0, column=2, sticky="W")
 
         self.search_engines = [
             SearchEngine(engine["name"], engine["search str"], engine["category"])
@@ -52,6 +61,10 @@ class Gui(tk.Tk):
             CategoryFrame(cats_frame, cat, engines).pack(
                 side=tk.LEFT, ipadx=10, padx=10
             )
+
+        style = ttk.Style(self)
+        style.configure("Search.TButton", font=("Open Sans", 15))
+        style.configure("Clear.TButton", font=("Open Sans", 12))
 
     def _on_clear(self):
         for engine in self.search_engines:
--- utils/category_frame.py
+++ utils/category_frame.py
@@ -19,12 +19,12 @@ class CategoryFrame(ttk.LabelFrame):
             ).pack(anchor="w", padx=2, pady=2)
 
         self.select_btn = ttk.Button(
-            self, style="Button.TButton", text="Select All", command=self._on_select
+            self, style="Select.TButton", text="Select All", command=self._on_select
         )
         self.select_btn.pack(padx=10, pady=10)
 
         style = ttk.Style(self)
-        style.configure("Button.TButton", font=("Helvetica", 12))
+        style.configure("Select.TButton", font=("Helvetica", 12))
         style.configure("Check.TCheckbutton", font=("Helvetica", 11))
 
     def _on_select(self):
